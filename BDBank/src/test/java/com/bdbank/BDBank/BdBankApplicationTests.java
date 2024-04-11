cd8cc930cbb5a4a318dfd8648c24c04c703ab1a3
Chukwuma Ibezim <123107253+cibezim@users.noreply.github.com>
2024-04-11 13:35:09 -0400

    feat: adds entity package

---
+++ BDBank/src/test/java/com/bdbank/BDBank/BdBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdbank.BDBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
